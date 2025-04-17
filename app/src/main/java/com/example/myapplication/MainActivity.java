f62b425cde4ccd2664c33a8d1885b42358bc972e
eliya <eliyacaspi4@gmail.com>
2025-04-17 19:58:41 +0300

    profile change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +20,22 @@ public class MainActivity extends AppCompatActivity {
         ImageButton breakfast_button=findViewById(R.id.food_breakfast_button);
         ImageButton lunch_button=findViewById(R.id.food_lunch_button);
         ImageButton dinner_button=findViewById(R.id.food_dinner_button);
+        EditText searchBar = findViewById(R.id.search_exercises);
+        //מימוש הסארצ באר ומעבר אימפרומנציה חיפוש לריסיקל
+        searchBar.setOnEditorActionListener((v, actionId, event) -> {
+            String query = searchBar.getText().toString().trim();
+            if (!query.isEmpty()) {
+                Intent intent = new Intent(MainActivity.this, ExercisieLay.class);
+                intent.putExtra("search_query", query);
+                startActivity(intent);
+            }
+            return true;
+        });
 
         profile_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Profile.class);
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
                 startActivity(intent);
 
             }
--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,13 +1,18 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
     private TextView usernameTextView;
+    private ImageView imageProfile;
     private TextView userDescriptionTextView;
     private FirebaseDatabaseService databaseService;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,14 @@ public class ProfileActivity extends AppCompatActivity {
         // Initialize views
         usernameTextView = findViewById(R.id.username);
         userDescriptionTextView = findViewById(R.id.userDescription);
+        imageProfile=findViewById(R.id.imageView2);
+        imageProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
         // Initialize Firebase service
         databaseService = FirebaseDatabaseService.getInstance();
